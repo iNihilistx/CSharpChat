d2817e686b673e47fe6421f77c6dc940c4108f0e
iNihilist <64751372+iNihilistx@users.noreply.github.com>
2025-03-08 03:37:06 +0000

    Add files via upload

---
+++ ChatApplication/Server/Client/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class ClientSocket
+{
+    private string ip;
+    private int port;
+    private Socket clientSocket;
+
+    public string Username;
+    public string IpAddress
+    {
+        get { return ip; }
+        set { ip = value; }
+    }
+    public int Port
+    {
+        get { return port; }
+        set { port = value; }
+    }
+
+    public ClientSocket(string username, string ipaddress, int portnumber)
+    {
+        Username = username;
+        IpAddress = ipaddress;
+        Port = portnumber;
+    }
+
+    public async Task Start()
+    {
+        clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        await clientSocket.ConnectAsync(new IPEndPoint(IPAddress.Parse(IpAddress), Port));
+        Console.WriteLine($"Connected to server: {IpAddress} : {Port}");
+
+        _ = Task.Run(() => ReceiveMessages());
+        
+        while (true)
+        {
+            Console.Write("\nStart Chatting: ");
+            string message = Console.ReadLine();
+            string UsernameMessage = $"{Username}: {message}";
+            if(string.IsNullOrEmpty(message))
+            {
+                Console.WriteLine("Message cannot be empty");
+                continue;
+            }
+            else if(message.ToLower() == "exit")
+            {
+                await SendMessage($"{Username} has left the chat");
+                break;
+            }
+            else
+            {
+                await SendMessage(UsernameMessage);
+            }
+        }
+    }
+
+    private async Task SendMessage(string message)
+    {
+        try
+        {
+            var buffer = Encoding.UTF8.GetBytes(message);
+            await clientSocket.SendAsync(buffer, SocketFlags.None);
+
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
+            Console.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.WriteLine($"{message}");
+        }
+        catch(Exception ex)
+        {
+            Environment.Exit(0);
+        }
+    }
+
+    private async Task ReceiveMessages()
+    {
+        while(true)
+        {
+            var buffer = new byte[1024];
+            var received = clientSocket.Receive(buffer);
+            var message = Encoding.UTF8.GetString(buffer, 0, received);
+            Console.WriteLine($"{message}");
+        }
+    }
+}
+
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        ClientSocket client = new ClientSocket("user", "0.0.0.0", 8080);
+        await client.Start();
+    }
+}
---
+++ ChatApplication/Server/Server/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Collections.Generic;
+
+class Server
+{
+    private HashSet<Socket> clients = new HashSet<Socket>();
+    private string ip;
+    private int port;
+    private Socket listener;
+    private bool isRunning = true; 
+
+    public string IpAddress
+    {
+        get { return ip; }
+        set { ip = value; }
+    }
+    public int Port
+    {
+        get { return port; }
+        set { port = value; }
+    }
+
+    public Server(string ipaddress, int portnumber)
+    {
+        IpAddress = ipaddress;
+        Port = portnumber;
+    }
+
+    public async Task Start()
+    {
+        listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        listener.Bind(new IPEndPoint(IPAddress.Parse(IpAddress), Port));
+        listener.Listen(10);
+        Console.WriteLine($"Server started on: {IpAddress} : {Port}");
+
+        while(isRunning)
+        {
+            var ClientSocket = await listener.AcceptAsync();
+            clients.Add(ClientSocket);
+            Console.WriteLine("Client connected");
+
+            // Handle the client in another thread, to allow for other clients to be accepted
+            _ = Task.Run(() => HandleClient(ClientSocket));
+        }
+    }
+
+    private async Task HandleClient(Socket client)
+    {
+        var buffer = new byte[1024];
+        try
+        {
+            while(isRunning)
+            {
+                var received = await client.ReceiveAsync(buffer, SocketFlags.None);
+                if (received == 0) break;
+
+                var response = Encoding.UTF8.GetString(buffer, 0, received);
+                Console.WriteLine(response);
+                Broadcast(response, client);
+            }
+        }
+        catch(Exception e)
+        {
+           // Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            client.Close();
+            clients.Remove(client);
+            //Console.WriteLine("Client Disconnected");
+        }
+    }
+
+    private async void Broadcast(string message, Socket sender )
+    {
+        foreach(var client in clients)
+        {
+            if(client != sender)
+            {
+                var messageBytes = Encoding.UTF8.GetBytes(message);
+                await client.SendAsync(messageBytes, SocketFlags.None);
+            }
+        }
+    }
+}
+
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        Server server = new Server("0.0.0.0", 8080);
+        await server.Start();
+    }
+}
